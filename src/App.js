d1f2bd30090c6d3ce66d7b675e866188a201edb7
H. alrahmani <h.m.alrahmani@gmail.com>
2022-05-18 16:57:55 +0300

    task 11 -the challange

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ChatRoom from "./components/ChatRoom";
@@ -8,12 +9,46 @@ import { Route, Routes } from "react-router-dom";
 const App = () => {
   const [rooms, setRooms] = useState([]);
 
-  const createRoom = (newRoom) => {
-    // to do : call BE to create a room
+  const fetchRoom = async () => {
+    try {
+      const response = await axios.get(
+        "https://coded-task-axios-be.herokuapp.com/rooms"
+      );
+      console.log(response.data);
+      setRooms(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const deleteRoom = (id) => {
+  useEffect(() => {
+    fetchRoom();
+  }, []);
+
+  const createRoom = async (newRoom) => {
+    // to do : call BE to create a room
+    try {
+      const response = await axios.post(
+        "https://coded-task-axios-be.herokuapp.com/rooms",
+        newRoom
+      );
+      setRooms([...rooms, response.data]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteRoom = async (id) => {
     // to do : call BE to delete a room
+    try {
+      const response = await axios.delete(
+        `https://coded-task-axios-be.herokuapp.com/rooms/${id}`
+      );
+      const newRoom = rooms.filter((room) => room.id !== id);
+      setRooms([...newRoom]);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -25,7 +60,17 @@ const App = () => {
               path="/room/:roomSlug"
               element={<ChatRoom rooms={rooms} />}
             />
-            <Route exact path="/" element={<ChatRoomsList rooms={rooms} />} />
+            <Route
+              exact
+              path="/"
+              element={
+                <ChatRoomsList
+                  rooms={rooms}
+                  createRoom={createRoom}
+                  deleteRoom={deleteRoom}
+                />
+              }
+            />
           </Routes>
         </center>
       </div>
